edab27fd04f55a0cb0dabbeece5ceab9f6a55108
Victor <vhlongon@gmail.com>
2020-12-17 12:07:53 +0100

    Add interface for return data

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 require('isomorphic-fetch');
 
-const url = 'https://swapi.dev/api/films';
+const url = 'https://swapi.dev/api/films/1';
 
+interface Film {
+  director: string;
+  title: string;
+  episode_id: number;
+}
 const fetchData = async () => {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  const { title, episode_id, director } = data as Film;
 
+  console.log(`
+  The director is: ${director}
+  The title is : ${title}
+  The episode id is: ${episode_id}
+  `);
   return data;
 };
 
